c293a3b1bb5ca717552cd3e4ac220c487ea31725
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-02-03 09:33:20 +0530

    parsed JSON to a const variable and fetched required data from it into another variables

--- app.js
+++ app.js
@@ -12,9 +12,17 @@ app.get("/", function(req, res){
 	const url = "https://api.openweathermap.org/data/2.5/weather?q=gaya&appid=83733882691fd4fbe45511863649a8c2&units=metric";
 
 	https.get(url, function(response){
-		console.log(response);
+		console.log(response.statusCode);
+
+		response.on("data", function(data){
+			const weatherData = JSON.parse(data);
+			const temp = weatherData.main.temp;
+			const desc = weatherData.weather[0].description;
+		})
 	})
-	
+
+
+
 	res.send("server running");
 });
 
