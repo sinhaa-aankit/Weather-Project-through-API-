37fb061b925b8657389b8a5a453a84914ac184e6
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-02-03 08:56:16 +0530

    added a API request to the app.js to request data from api

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const app = express();
 
 app.get("/", function(req, res){
 
+	const url = "https://api.openweathermap.org/data/2.5/weather?q=gaya&appid=83733882691fd4fbe45511863649a8c2&units=metric";
+
+	https.get(url, function(response){
+		console.log(response);
+	})
 	
 	res.send("server running");
 });
