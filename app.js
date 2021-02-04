c192196776c9609a340f0fcd368537e49e013146
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-02-04 08:38:09 +0530

    installed package body-parser and added input type in the index.html to take input of the city name and fetch weather condition. sent back the index.html as response from app.js

--- app.js
+++ app.js
@@ -9,28 +9,33 @@ const app = express();
 
 app.get("/", function(req, res){
 
-	const url = "https://api.openweathermap.org/data/2.5/weather?q=gaya&appid=83733882691fd4fbe45511863649a8c2&units=metric";
-
-	https.get(url, function(response){
-		console.log(response.statusCode);
-
-		response.on("data", function(data){
-			const weatherData = JSON.parse(data);
-			const temp = weatherData.main.temp;
-			const desc = weatherData.weather[0].description;
-			const icon = weatherData.weather[0].icon;
-			const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-			res.write("<h1> The weather in Gaya is currently " + desc + "</h1> <img src="+imageURL+ ">");
-			res.write(" <br><h1>The temperature in Gaya is " + temp + " degree Celcious </h1>");
-			// res.write("<img src="+ imageURL +">");
-			res.send();
-		})
-	})
+	res.sendFile(__dirname + "/index.html");
 
 
 
 	// res.send("server running");
 });
+
+// const city = "Gaya";
+// 	const apiKey = "83733882691fd4fbe45511863649a8c2";
+
+// 	const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + apiKey +"&units=metric";
+
+// 	https.get(url, function(response){
+// 		console.log(response.statusCode);
+
+// 		response.on("data", function(data){
+// 			const weatherData = JSON.parse(data);
+// 			const temp = weatherData.main.temp;
+// 			const desc = weatherData.weather[0].description;
+// 			const icon = weatherData.weather[0].icon;
+// 			const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+// 			res.write("<h1> The weather in Gaya is currently " + desc + "</h1> <img src="+imageURL+ ">");
+// 			res.write(" <br><h1>The temperature in Gaya is " + temp + " degree Celcious </h1>");
+// 			// res.write("<img src="+ imageURL +">");
+// 			res.send();
+// 		})
+// 	})
 
 
 
