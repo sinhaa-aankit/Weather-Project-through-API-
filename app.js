7f5b4966e6fb0735f4bcfa62b8d18dcc29ae74e7
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-02-04 08:44:55 +0530

    Now api will fetch the weather of the city given by user and will show it with image according to the weather condition

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 
@@ -11,31 +14,34 @@ app.get("/", function(req, res){
 
 	res.sendFile(__dirname + "/index.html");
 
-
-
 	// res.send("server running");
 });
 
-// const city = "Gaya";
-// 	const apiKey = "83733882691fd4fbe45511863649a8c2";
+app.post("/", function(req, res){
+	const cityName = req.body.cityName;
+	const city = cityName;
+	const apiKey = "83733882691fd4fbe45511863649a8c2";
 
-// 	const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + apiKey +"&units=metric";
+	const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + apiKey +"&units=metric";
 
-// 	https.get(url, function(response){
-// 		console.log(response.statusCode);
+	https.get(url, function(response){
+		console.log(response.statusCode);
 
-// 		response.on("data", function(data){
-// 			const weatherData = JSON.parse(data);
-// 			const temp = weatherData.main.temp;
-// 			const desc = weatherData.weather[0].description;
-// 			const icon = weatherData.weather[0].icon;
-// 			const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-// 			res.write("<h1> The weather in Gaya is currently " + desc + "</h1> <img src="+imageURL+ ">");
-// 			res.write(" <br><h1>The temperature in Gaya is " + temp + " degree Celcious </h1>");
-// 			// res.write("<img src="+ imageURL +">");
-// 			res.send();
-// 		})
-// 	})
+		response.on("data", function(data){
+			const weatherData = JSON.parse(data);
+			const temp = weatherData.main.temp;
+			const desc = weatherData.weather[0].description;
+			const icon = weatherData.weather[0].icon;
+			const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+			res.write("<h1> The weather in "+ city +" is currently " + desc + "</h1> <img src="+imageURL+ ">");
+			res.write(" <br><h1>The temperature in "+ city +" is " + temp + " degree Celcious </h1>");
+			// res.write("<img src="+ imageURL +">");
+			res.send();
+		})
+	})
+});
+
+
 
 
 
